cec6367b2388838b7059eb779c06d7785cae51eb
angelstchavez <angelstchavez@gmail.com>
2025-03-17 10:59:23 -0500

    Actualizar la interfaz de la página principal y agregar la dependencia framer-motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4 text-green-700">
+        Parcial N° 1 - Modelos y Simulación
+      </h1>
+      <p className="text-xl">Seleccione una de las opciones del menú</p>
+    </div>
+  );
 }
