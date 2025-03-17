350a58762b6782240294fb15a81cc330403f0552
angelstchavez <angelstchavez@gmail.com>
2025-03-17 11:06:59 -0500

    Actualizar el diseño del formulario: agregar una columna adicional en la cuadrícula para mejorar la disposición de los campos

--- components/main/custom-form.tsx
+++ components/main/custom-form.tsx
@@ -110,7 +110,7 @@ const CustomForm = ({
             onSubmit={form.handleSubmit(handleSubmit)}
             className="space-y-4"
           >
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {fields.map((field) => (
                 <FormField
                   key={field.name}
