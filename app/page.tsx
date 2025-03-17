3e3807c78c5ecdca46862b9fd8b1e9bbe0059ba5
angelstchavez <angelstchavez@gmail.com>
2025-03-17 12:09:41 -0500

    Actualizar el diseño de la página principal: implementar tarjeta para mejorar la presentación del contenido

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4 text-green-700">
-        Parcial N° 1 - Modelos y Simulación
-      </h1>
-      <p className="text-xl">Seleccione una de las opciones del menú</p>
-      <p className="text-sm">Desarollado por: Angel Chavez - Juan Pineda</p>
+    <div className="mx-auto p-4 justify-center min-h-screen">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-4xl font-bold mb-4 text-green-700">
+            Parcial N° 1 - Modelos y Simulación
+          </CardTitle>
+          <CardDescription className="text-xl">
+            Seleccione una de las opciones del menú
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <p className="text-sm">
+            <span className="font-semibold">Desarollado por:</span> Angel Chavez
+            - Juan Pineda
+          </p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
